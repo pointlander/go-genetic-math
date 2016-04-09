acfd0dbd5af76241b9d3018929cd40feb2eabde1
rogeralsing <rogeralsing@gmail.com>
2016-04-09 14:08:29 +0200

    print cases

--- Program.go
+++ Program.go
@@ -7,6 +7,17 @@ import (
 )
 
 func main() {
+    cases := engine.Cases(engine.Case(20, engine.Input("x",10),engine.Input("y",0)))
+    for x,c := range cases.Cases {
+        fmt.Printf("Case %v", x)
+        println()
+        println(c.Result)
+        for _,i := range c.Inputs {
+            fmt.Printf("Var %s = %v",i.Variable,i.Value)
+            println()
+        }
+    }
+
 	context := engine.NewContext()
 	context.SetVariable("x", 10)
 	add := ast.Add(ast.Mul(ast.Literal(10.111), ast.Literal(20)), ast.Literal(20))
--- engine/Context.go
+++ engine/Context.go
@@ -1,8 +1,5 @@
 package engine
 
-import (
-)
-
 type Context struct {
 	variables map[string]float64
 }
@@ -19,4 +16,33 @@ func (node *Context) GetVariable(variable string) float64 {
 
 func (node *Context) SetVariable(variable string, value float64) {
 	node.variables[variable] = value
+}
+
+type InputValue struct {
+	Variable string
+	Value    float64
+}
+
+func Input(variable string, value float64) InputValue {
+	return InputValue{Variable: variable, Value: value}
+}
+
+type CaseValue struct {
+	Inputs []InputValue
+	Result float64
+}
+
+func Case (result float64, inputs ...InputValue) CaseValue  {
+    return CaseValue {
+        Result: result,
+        Inputs: inputs,
+        }
+}
+
+type CasesValue struct {
+	Cases []CaseValue
+}
+
+func Cases(cases ...CaseValue) CasesValue {
+    return CasesValue { Cases: cases}
 }
