bb06cd150d2a8326d93d3039912878f63555e42e
rogeralsing <rogeralsing@gmail.com>
2016-04-11 13:54:30 +0200

    eval fitness

--- Program.go
+++ Program.go
@@ -8,13 +8,17 @@ import (
 )
 
 func main() {
-	cases := engine.Cases(engine.Case(20, engine.Input("x", 10), engine.Input("y", 0)),
-		engine.Case(20, engine.Input("x", 10), engine.Input("y", 0)))
+	cases := engine.Cases(engine.Case(10, engine.Input("x", 5), engine.Input("y", 2)),
+		engine.Case(20, engine.Input("x", 10), engine.Input("y", 2)),
+		engine.Case(30, engine.Input("x", 10), engine.Input("y", 3)))
 
 	fmt.Printf("%+v", cases)
+
+	add := ast.Add(ast.Mul(ast.Literal(10.111), ast.Literal(20)), ast.Literal(20))
+
 	context := engine.NewContext()
 	context.SetVariable("x", 10)
-	add := ast.Add(ast.Mul(ast.Literal(10.111), ast.Literal(20)), ast.Literal(20))
+
 	add = add.Mutate()
 	fmt.Printf("%v", add)
 }
---
+++ world/cases.go
@@ -0,0 +1,63 @@
+package world
+
+import (
+	"bytes"
+	"fmt"
+    "github.com/rogeralsing/GoMath/ast"
+	"github.com/rogeralsing/GoMath/engine"
+)
+
+type InputValue struct {
+	Variable string
+	Value    float64
+}
+
+func Input(variable string, value float64) InputValue {
+	return InputValue{Variable: variable, Value: value}
+}
+
+type CaseValue struct {
+	Inputs []InputValue
+	Result float64
+}
+
+func Case(result float64, inputs ...InputValue) CaseValue {
+	return CaseValue{
+		Result: result,
+		Inputs: inputs,
+	}
+}
+
+type CasesValue struct {
+	Cases []CaseValue
+}
+
+func Cases(cases ...CaseValue) CasesValue {
+	return CasesValue{Cases: cases}
+}
+
+func (cases CasesValue) String() string {
+	var buffer bytes.Buffer
+	for x, c := range cases.Cases {
+		buffer.WriteString(fmt.Sprintf("Case %v = %v \n", x, c.Result))
+		for _, i := range c.Inputs {
+			buffer.WriteString(fmt.Sprintf("\tVar %s = %v \n", i.Variable, i.Value))
+		}
+	}
+	return fmt.Sprint(buffer.String())
+}
+
+func (cases CasesValue) Eval(node ast.Node) float64 {
+    context := engine.NewContext()
+    total := 0.0
+    for _,c := range cases.Cases {
+        for _, i := range c.Inputs {
+            context.SetVariable(i.Variable,i.Value)
+        }
+        res := node.Eval(context)
+        diff := c.Result - res
+        total += diff
+    } 
+    
+    return total
+}
